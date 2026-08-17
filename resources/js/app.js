2ad1773d7579e67e137d1e64a666679e09de2481
mahdi nobahar <nobahar.mahdi@yahoo.com>
2026-08-17 16:30:30 +0330

    8/17/2026 - 4:30

---
+++ resources/js/app.js
@@ -0,0 +1,11 @@
+new Swiper('.portfolio-swiper', {
+    slidesPerView: 3,
+    spaceBetween: 30,
+    pagination: {
+        clickable: true,
+    },
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+});
